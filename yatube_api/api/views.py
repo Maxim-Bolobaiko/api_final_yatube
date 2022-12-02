071ab1d2595f8b3f72cdfd169b9bebf4df19e032
Максим Болобайко <maxim.bolobaiko@yandex.ru>
2022-12-02 13:22:13 +0300

    second_review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 
-from posts.models import Group, Post, Follow
+from posts.models import Group, Post
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (
     CommentSerializer,
